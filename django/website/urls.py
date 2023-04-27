dc085e7ac25b07acd8dbbbf250a62929a63d7d27
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-04-27 13:54:26 -0300

    restart

--- django/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'website'
-
-urlpatterns=[
-    path("api/", views.SevicesListView.as_view(), name="website_home"),
-]
--- django/website/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import Services
-from .serializers import ServicesSerializer
-
-class SevicesListView(generics.ListAPIView):
-    queryset = Services.objects.all()
-    serializer_class = ServicesSerializer
