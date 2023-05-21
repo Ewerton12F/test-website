a97eb65677af1c90e72de11aa9250496f9a90afd
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-21 20:30:01 -0300

    test

---
+++ src/app/page.spec.tsx
@@ -0,0 +1,9 @@
+import Home from './page';
+
+import { render } from '@testing-library/react';
+
+test('sum', () => {
+  const { getByText } = render(<Home />);
+
+  expect(getByText(`Hello World!`)).toBeInTheDocument;
+});
