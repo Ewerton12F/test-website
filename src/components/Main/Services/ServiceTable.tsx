a9c4c8e188933e854316d8991e3339e94fe1e2da
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-30 20:40:22 -0300

    swiper grid

--- src/components/Main/Services/ServiceTable.tsx
+++ src/components/Main/Services/ServiceTable.tsx
@@ -2,32 +2,118 @@
 // register Swiper custom elements
 register();
 
-import getAllServices from '../../../../lib/getAllServices';
+import React, { useEffect, useState } from 'react';
 
+import { Service } from '../../../../types';
+
+import useFetch from '@/hooks/useFetch';
 import { Icon3dCubeSphere } from '@tabler/icons-react';
-import { Pagination, Virtual } from 'swiper';
+import { Grid, Navigation, Pagination } from 'swiper';
 import { register } from 'swiper/element/bundle';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/css';
+import 'swiper/css/grid';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import 'swiper/css/virtual';
 
-export default async function ServiceTable(props: any) {
-  const servicesData = getAllServices();
-  const services = await servicesData;
+export default function ServiceTable(props: any) {
+  const [services, setServices] = useState<Service[]>([]);
 
   const coners: string[][] = [['br'], ['bl'], ['tr'], ['tl']];
 
-  let oneservice: any = [];
-  const fourservices: any = [];
+  useEffect(() => {
+    fetch('https://teste-server.vercel.app/api/services-list')
+      .then((response) => response.json())
+      .then((data) => setServices(data));
+  }, []);
 
-  const qttslides: number = Math.ceil(services.length / 4);
+  console.log(services);
+  return (
+    <Swiper
+      className="w-[100%] h-[100%]"
+      slidesPerView={2}
+      spaceBetween={10}
+      grid={{
+        rows: 2,
+        fill: 'row'
+      }}
+      pagination={{
+        clickable: true
+      }}
+      modules={[Grid, Pagination]}
+    >
+      {services.map((service) => {
+        return (
+          <SwiperSlide key={service.id} className="w-1/2">
+            <div
+              className={`
+                      m-2 border md:flex
+                      border-primary pt-5 md:p-6
+                      rounded-${coners[service.id % coners.length]}-none
+                      rounded-lg
+                    `}
+            >
+              <div
+                className={`
+                        flex mx-auto md:m-0 md:min-w-[96px]
+                        h-14 w-14 md:h-24 md:w-24
+                        items-center justify-center 
+                        rounded-xl bg-primary
+                      `}
+              >
+                <Icon3dCubeSphere color="#FFC759" size={48} strokeWidth={1} />
+              </div>
+              <div className="md:ml-5 md:w-100%">
+                <div
+                  className={`
+                          flex md:flex-col
+                          h-14 
+                          w-auto 
+                          items-center md:items-start
+                          justify-center md:justify-start
+                        `}
+                >
+                  <h4
+                    className={`
+                              mx-auto md:mx-0
+                              text-sm md:text-2xl 
+                              font-jost capitalize text-primary
+                            `}
+                  >
+                    {service.title}
+                  </h4>
+                  <p
+                    className={`
+                              hidden md:flex flex-wrap
+                              font-Lexend text-left 
+                              text-sm normal-case text-primary
+                            `}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+}
+/*       
+      <li >
+        
+      </li>
+*/
 
-  const count = 0;
+/* 
 
-  for (let i = 0; i <= services.length; i++) {
+  let oneservice: Service[] = [];
+  const fourservices: Service[][] = [];
+
+  for (let i = 0; i <= services?.length; i++) {
     oneservice.push(services[i]);
 
     if (oneservice.length === 4) {
@@ -42,7 +128,7 @@ export default async function ServiceTable(props: any) {
     <>
       <Swiper
         // install Swiper modules
-        modules={[Pagination, Virtual]}
+        modules={[Pagination]}
         spaceBetween={50}
         slidesPerView={1}
         pagination={{ clickable: true }}
@@ -56,14 +142,9 @@ export default async function ServiceTable(props: any) {
           '--swiper-padding': '16px',
           '--swiper-pagination-bottom': '0px'
         }}
-        virtual
       >
         {fourservices.map((services: any) => (
-          <SwiperSlide
-            key={services}
-            virtualIndex={services.id}
-            className="mb-8"
-          >
+          <SwiperSlide key={services} className="mb-8">
             <ul className={`mx-3 flex flex-row flex-wrap`}>
               {services.map((service: any) => (
                 <li key={service.id} className={`w-1/2`}>
@@ -130,3 +211,4 @@ export default async function ServiceTable(props: any) {
     </>
   );
 }
+ */
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,6 @@
-import { Service } from '../types';
+import { Service } from '../../types';
 
-export default async function getAllServices(): Promise<Service[]> {
+export default async function useFetch(): Promise<Service[]> {
   const res = await fetch('https://teste-server.vercel.app/api/services-list');
 
   if (!res.ok) throw new Error('failed to fetch data');
