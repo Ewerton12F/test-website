533440df8e0ffe0997f31411c2dd789b3959f1a7
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-07-21 13:00:07 -0300

    photo-desc-fix: alt description enhanced

--- src/components/Main/Profile/Profile.tsx
+++ src/components/Main/Profile/Profile.tsx
@@ -24,7 +24,7 @@ export default function Profile() {
             src="/profile-photo.jpeg"
             width={7000}
             height={7000}
-            alt="Dr. Robson Batista - Psicólogo | Psicanalista"
+            alt="Dr. Róbson Batista - Psicólogo | Psicanalista"
           />
           <div className="mt-5 md:mt-0">
             <div className="hidden lg:block">
